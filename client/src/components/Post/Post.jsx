63abedf835e91a67a1ca299233310b8d12d169ca
Rachel Ramberg <rachelramberg@Rachels-MacBook-Pro.local>
2021-05-24 15:47:23 -0400

    added more rendered context

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,12 +1,16 @@
-
+import { Link } from "react-router-dom";
 
 const Post = () => {
-
   return (
     <div className="post">
-      <span>This is a post</span>
+      <h3>Title</h3>
+      <img src="" alt="blog-post-visual" />
+      <p>Content</p>
+      <p>
+        <em>- Author</em>
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Post;
--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,14 +1,15 @@
-import Layout from '../../components/Layout/Layout'
-import Post from '../../components/Post/Post'
+import Layout from "../../components/Layout/Layout";
+import Post from "../../components/Post/Post";
 
 const Posts = () => {
   return (
     <Layout>
       <main className="posts-container">
         <span>Posts Page</span>
+        <Post />
       </main>
     </Layout>
-  )
-}
+  );
+};
 
 export default Posts;
